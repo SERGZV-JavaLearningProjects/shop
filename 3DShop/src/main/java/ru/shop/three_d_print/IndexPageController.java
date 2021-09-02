7b9abd19a927c6f33dcffc58412eec736f6d92cb
Сергей <sergzv22@rambler.ru>
2021-09-02 19:12:43 +0300

    +

--- 3DShop/src/main/java/ru/shop/three_d_print/IndexPageController.java
+++ 3DShop/src/main/java/ru/shop/three_d_print/IndexPageController.java
@@ -4,8 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HelloController
+public class IndexPageController
 {
+    @GetMapping
+    public String test()
+    {
+        System.out.println("Дошло");
+        return "3d_shop";
+    }
+
     @GetMapping("/3d_shop")
     public String sayHello()
     {
