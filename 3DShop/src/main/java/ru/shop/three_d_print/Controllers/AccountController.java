d275dfcf967ca74356eaaa1a9309a975113d9187
Сергей <sergzv22@rambler.ru>
2021-09-24 16:08:11 +0300

    update_objects_fix
    Now, standing in any angle, the victim of war can take an object in his hand/hands and it will be displayed

--- 3DShop/src/main/java/ru/shop/three_d_print/Controllers/AccountController.java
+++ 3DShop/src/main/java/ru/shop/three_d_print/Controllers/AccountController.java
@@ -2,13 +2,15 @@ package ru.shop.three_d_print.Controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class MainController
+@RequestMapping("/account")
+public class AccountController
 {
     @GetMapping
-    public String index()
+    public String myAccount()
     {
-        return "main/index";
+        return "account/my-account";
     }
 }
