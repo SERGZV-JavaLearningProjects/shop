82d0e73b7b4b4ba982f36cd9399bc8bf7c544350
Сергей <sergzv22@rambler.ru>
2021-09-27 19:20:02 +0300

    images_moved_back
    Moved back images to directory
    webapp / resources, also added this directory to the scope in java. So the pictures are displayed on the site and when the cursor is hovering, the path is displayed.

--- 3DShop/src/main/java/ru/shop/three_d_print/config/SpringConfig.java
+++ 3DShop/src/main/java/ru/shop/three_d_print/config/SpringConfig.java
@@ -6,9 +6,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.thymeleaf.spring5.ISpringTemplateEngine;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
@@ -19,6 +19,12 @@ import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 public class SpringConfig implements WebMvcConfigurer
 {
     private final ApplicationContext applicationContext;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry)
+    {
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+    }
 
     @Autowired
     public SpringConfig(ApplicationContext applicationContext)
