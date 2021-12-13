c252a50fe19a81e1b40f8a4b1b55ac8c287abe42
Сергей <31069610+SERGZV@users.noreply.github.com>
2021-12-13 17:26:11 +0300

    Profile creation implementation (#6)
    The profile in the database is now created through the form on the site.
    
    After creating a profile, we are redirected to a page that counts from 20 to zero and automatically redirects us to the newly created profile. We also have the opportunity to go to it manually by pressing the transition button.

--- 3DShop/src/main/java/ru/shop/three_d_print/dao/AccountDAO.java
+++ 3DShop/src/main/java/ru/shop/three_d_print/dao/AccountDAO.java
@@ -1,8 +1,9 @@
 package ru.shop.three_d_print.dao;
 
 import org.springframework.stereotype.Component;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 import ru.shop.three_d_print.models.Account;
-
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -12,7 +13,6 @@ public class AccountDAO
 {
     private static int ACCOUNT_ID = 0;
 
-//  jdbc:postgresql://localhost:5432/three_d_shop_db
     private static final String URL = "jdbc:postgresql://localhost:5432/three_d_shop_db";
     private static final String USERNAME = "postgres";
     private static final String PASSWORD = "666999Qw";
@@ -24,8 +24,7 @@ public class AccountDAO
         try
         {
             Class.forName("org.postgresql.Driver");
-        }
-        catch (ClassNotFoundException e)
+        } catch (ClassNotFoundException e)
         {
             e.printStackTrace();
         }
@@ -40,9 +39,91 @@ public class AccountDAO
         }
     }
 
-    public Account GetAccount(int id)
+    public List<ObjectError> checkForMatches(Account account)
     {
-        Account account = new Account();
+        List<ObjectError> errors = new ArrayList();
+        boolean emailError = false;
+        boolean loginError = false;
+
+        try
+        {
+            String SQL = "SELECT * FROM account WHERE email = ? OR login = ?";
+            PreparedStatement statement = connection.prepareStatement(SQL);
+            statement.setString(1, account.getEmail());
+            statement.setString(2, account.getLogin());
+            ResultSet resultSet = statement.executeQuery();
+
+            while (resultSet.next())
+            {
+                if(resultSet.getString("email").equals(account.getEmail()))
+                    emailError = true;
+
+                if(resultSet.getString("login").equals(account.getLogin()))
+                    loginError = true;
+
+                if(emailError && loginError) break;
+            }
+
+            if (emailError)
+                errors.add(new ObjectError("email", "Email " + account.getEmail() + " already exists"));
+
+            if (loginError)
+                errors.add(new ObjectError("login", "Login " + account.getLogin() + " already exists"));
+        }
+        catch (SQLException exception)
+        {
+            exception.printStackTrace();
+        }
+
+        return errors;
+    }
+
+    public void createAccount(Account account)
+    {
+        try
+        {
+            String SQL = "INSERT INTO account(fname, mname, lname, age, sex, email, login, password) VALUES (?,?,?,?,?,?,?,?)";
+            PreparedStatement statement = connection.prepareStatement(SQL);
+            statement.setString(1, account.getFirstName());
+            statement.setString(2, account.getMiddleName());
+            statement.setString(3, account.getLastName());
+            statement.setInt(4, account.getAge());
+            statement.setString(5, account.getSex());
+            statement.setString(6, account.getEmail());
+            statement.setString(7, account.getLogin());
+            statement.setString(8, account.getPassword());
+            statement.executeUpdate();
+        }
+        catch (SQLException exception)
+        {
+            exception.printStackTrace();
+        }
+    }
+
+    public int getLastAccountId()
+    {
+        try
+        {
+            Statement statement = connection.createStatement();
+            String SQL = "SELECT MAX(id) FROM account;";
+            ResultSet resultSet = statement.executeQuery(SQL);
+
+            if(resultSet.next())
+            {
+                 int id = resultSet.getInt(1);
+                 if(id != 0) return id;
+            }
+        }
+        catch (SQLException exception)
+        {
+            exception.printStackTrace();
+        }
+        return 0;
+    }
+
+    public Account getAccount(int id)
+    {
+        Account account = null;
 
         try
         {
@@ -54,19 +135,22 @@ public class AccountDAO
             {
                 if(resultSet.getInt("id") == id)
                 {
+                    account = new Account();
                     account.setId(id);
                     account.setFirstName(resultSet.getString("fname"));
                     account.setMiddleName(resultSet.getString("mname"));
                     account.setLastName(resultSet.getString("lname"));
                     account.setAge(resultSet.getInt("age"));
-                    account.setGender(resultSet.getString("gender"));
+                    account.setSex(resultSet.getString("sex"));
                     account.setEmail(resultSet.getString("email"));
+                    account.setLogin(resultSet.getString("login"));
+                    account.setPassword(resultSet.getString("password"));
                 }
             }
         }
-        catch (SQLException throwables)
+        catch (SQLException exception)
         {
-            throwables.printStackTrace();
+            exception.printStackTrace();
         }
 
         return account;
--- 3DShop/src/main/java/ru/shop/three_d_print/сontrollers/AccountsController.java
+++ 3DShop/src/main/java/ru/shop/three_d_print/сontrollers/AccountsController.java
@@ -3,10 +3,15 @@ package ru.shop.three_d_print.сontrollers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.*;
 import ru.shop.three_d_print.dao.AccountDAO;
+import ru.shop.three_d_print.models.Account;
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Objects;
 
 @Controller
 @RequestMapping("/account")
@@ -20,7 +25,32 @@ public class AccountsController
     @GetMapping("/{id}")
     public String showAccount(@PathVariable int id, Model model)
     {
-        model.addAttribute("account", accountDAO.GetAccount(id));
-        return "account/show_account";
+        model.addAttribute("account", accountDAO.getAccount(id));
+        return "account/show";
+    }
+
+    @GetMapping("/new")
+    public String createAccount(Model model)
+    {
+        model.addAttribute("account", new Account());
+        return "account/create";
+    }
+
+    @PostMapping("/created")
+    public String accountCreated(@ModelAttribute @Valid Account account, BindingResult bindingResult, Model model)
+    {
+        if(bindingResult.hasErrors()) return "account/create";
+
+        List<ObjectError> errors = accountDAO.checkForMatches(account);
+
+        for (ObjectError error : errors)
+            bindingResult.rejectValue(error.getObjectName(), "", Objects.requireNonNull(error.getDefaultMessage()));
+
+        if(bindingResult.hasErrors()) return "account/create";
+
+        accountDAO.createAccount(account);
+        int id = accountDAO.getLastAccountId();
+        model.addAttribute("id", id);
+        return "account/created";
     }
 }
