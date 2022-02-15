20e9248775e20edfcbbcf25bc85527347b5fdd1d
Сергей <31069610+SERGZV@users.noreply.github.com>
2022-02-15 18:37:03 +0400

    Product page (#15)
    * backup
    
    * backup
    
    Made drawing an indefinite number of thumbnails when you click on one of which it is shown in the main window
    
    * task_completed
    
    The product page is ready and working in test mode, displaying only one manually written product in the code. In order to display the products recorded in the database in the next branch, I need to add tables that will store these products in the database.

--- 3DShop/src/main/java/ru/shop/three_d_print/сontrollers/AccountsController.java
+++ 3DShop/src/main/java/ru/shop/three_d_print/сontrollers/AccountsController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
 import ru.shop.three_d_print.dao.AccountDAO;
