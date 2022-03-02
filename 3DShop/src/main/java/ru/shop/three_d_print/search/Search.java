ede5e2dd06945f1e3a6bdad18e642a7813ed1638
Сергей <sergzv22@rambler.ru>
2022-03-02 11:41:51 +0400

    backup_before_trying_a_new_resource_access_approach

---
+++ 3DShop/src/main/java/ru/shop/three_d_print/search/Search.java
@@ -0,0 +1,40 @@
+package ru.shop.three_d_print.search;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.io.File;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Search
+{
+    public List<String> getDirectoryFileNames(String suffixPath)
+    {
+        Class thisClass = getClass();
+        URL url = thisClass.getClassLoader().getResource(suffixPath);
+        String correctedPath = url.getPath().replace("%20", " ");
+        File folder = new File(correctedPath);
+        Path path = Paths.get(folder.getPath());
+
+        if (!Files.exists(path)) throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Internal server error");
+
+        File[] files = folder.listFiles();
+        List<String> fileNames = new ArrayList();
+
+        if (files != null)
+        {
+            for(File file : files)
+            {
+                System.out.println("Очередной файл был наден: " + file.getName());
+                fileNames.add(file.getName());
+            }
+        }
+
+        return fileNames;
+    }
+}
--- 3DShop/src/main/java/ru/shop/three_d_print/сontrollers/ProductController.java
+++ 3DShop/src/main/java/ru/shop/three_d_print/сontrollers/ProductController.java
@@ -9,14 +9,7 @@ import org.springframework.web.server.ResponseStatusException;
 import ru.shop.three_d_print.dao.ProductDAO;
 import ru.shop.three_d_print.models.Order;
 import ru.shop.three_d_print.models.Product;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import ru.shop.three_d_print.search.Search;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -36,42 +29,16 @@ public class ProductController
     @GetMapping("/{id}")
     public String showItem(@PathVariable int id, Model model)
     {
-        // Новый тестовый код
-        System.out.println("До !!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        Product product = productDAO.getProduct(id);
+        if(product == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Page not found");
 
-        Product testProduct = productDAO.getProduct(id);
-        if(testProduct == null) throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Page not found");
-
-//        readFile("images/products/" + id);
-        List<String> imageNames = getDirectoryFileNames("images/products/" + id);
-
-        System.out.println("После !!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-
-        //....
-
-        // Старый код
-        Product product = new Product();
-        product.setName("ABS plastic 1,75 SEM blue");
-        product.setDescription
-        (
-            "The domestic trademark SEM presents to your attention a classic plastic for 3D printing" +
-            " - ABS plastic 1.75 SEM blue. This is a practical and affordable material that" +
-            " can be used to print various designs, body parts, souvenirs and medical supplies," +
-            " car parts and various devices.\n" +
-            "\n" +
-            "This plastic is a universal consumable material that can be used in almost all 3D" +
-            " printers that print using FDM technology, both expensive and quite budget models.\n" +
-            "\n" +
-            "The blue ABS plastic SEM is characterized by high impact and heat resistance."
-        );
-
-        product.setPrice(19.25f);
+        Search search = new Search();
+        List<String> imageNames = search.getDirectoryFileNames("images/products/" + id);
 
         List<String> imageLinks = new ArrayList<>();
-//        imageLinks.add("/E:/Other Files/Development/Java Server/shopRepository/3DShop/target/shop/WEB-INF/classes/images/products/1/1.jpg");
-        imageLinks.add("/resources/static/images/products/1/1.jpg");
-        imageLinks.add("/resources/static/images/products/1/korobka.png");
-        imageLinks.add("/resources/static/images/products/1/primer-plastika-sem-cvet-sinij.jpg");
+
+        for (String imageName : imageNames)
+            imageLinks.add("/resources/static/images/products/" + id + "/" + imageName);
 
         product.setImages(imageLinks);
 
@@ -81,7 +48,6 @@ public class ProductController
 
         model.addAttribute("order", order);
         return "product/product";
-        //
     }
 
     @PostMapping("/add-to-cart")
@@ -92,73 +58,4 @@ public class ProductController
         model.addAttribute("order", order);
         return "product/product";
     }
-
-//    private void readFile(String fileName)
-//    {
-//        System.out.println("Запускаем метод readFile");
-//        Class testClass = getClass();
-//        URL url = testClass.getClassLoader().getResource(fileName);
-//        String stringPath = url.getPath().replace("%20", " ");
-//        File file = new File(stringPath);
-//        Path path = Paths.get(file.getPath());
-//
-//        if(Files.exists(path))
-//        {
-//            System.out.println("Путь существует");
-//            getDirectoryFileNames(file);
-//        }
-//        else
-//        {
-//            System.out.println("Путь не существует");
-//        }
-//    }
-
-    public List<String> getDirectoryFileNames(String clippedPath)
-    {
-        Class thisClass = getClass();
-        URL url = thisClass.getClassLoader().getResource(clippedPath);
-        String correctedPath = url.getPath().replace("%20", " ");
-        File folder = new File(correctedPath);
-        Path path = Paths.get(folder.getPath());
-
-        if (!Files.exists(path)) throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Internal server error");
-
-        File[] files = folder.listFiles();
-        List<String> fileNames = new ArrayList();
-
-        if (files != null)
-        {
-            for(File file : files)
-            {
-                System.out.println("Очередной файл был наден: " + file.getName());
-                fileNames.add(file.getName());
-            }
-        }
-
-        return fileNames;
-    }
-
-//    public List<String> getDirectoryFileNames(final File folder)
-//    {
-//        File[] files = folder.listFiles();
-//        List<String> fileNames = new ArrayList();
-//
-//        if (files != null)
-//        {
-//            for(File file : files)
-//            {
-//                if(file.isDirectory())
-//                {
-//                    getDirectoryFileNames(file);
-//                }
-//                else
-//                {
-//                    System.out.println("Директория или файл были наденны: " + file.getName());
-//                    fileNames.add(file.getName());
-//                }
-//            }
-//        }
-//
-//        return fileNames;
-//    }
 }
