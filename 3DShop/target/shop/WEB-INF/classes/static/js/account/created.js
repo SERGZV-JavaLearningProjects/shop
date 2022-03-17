31e9ea30e1ca8ec767e9e9ffdb2f1562c0684617
Сергей <sergzv22@rambler.ru>
2022-03-17 18:29:33 +0400

    transfer_javascript_2
    Moved javascript code from the product to a separate document

---
+++ 3DShop/target/shop/WEB-INF/classes/static/js/account/created.js
@@ -0,0 +1,19 @@
+let timer = 20;
+let timerId;
+
+function countdown()
+{
+    document.getElementById("_timer").innerHTML = timer.toString();
+    timerId = setInterval(function ()
+    {
+        timer--;
+        document.getElementById("_timer").innerHTML = timer.toString();
+
+        if(timer < 1)
+        {
+            clearInterval(timerId);
+            document.getElementById("redirect_to_profile").click();
+        }
+    }, 1000);
+}
+
